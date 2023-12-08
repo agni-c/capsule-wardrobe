61b4a6aae337e81320cd5c8f96c6b5a3eae4b368
agni <agnibha0016@gmail.com>
2023-12-08 19:40:38 +0530

    Delete unused assets and configuration files

--- client/src/components/index.js
+++
@@ -1,7 +0,0 @@
-import CustomButton from './CustomButtion'
-import ColorPicker from './ColorPicker'
-import FilePicker from './FilePicker'
-import AIPicker from './AIPicker'
-import Tab from './Tab'
-
-export { CustomButton, ColorPicker, FilePicker, AIPicker, Tab }
